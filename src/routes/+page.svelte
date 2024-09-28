afe5072e3788b7f04fd1ec85c38cbf5460dab032
EnisCiftci <eniscif@googlemail.com>
2024-09-28 19:16:05 +0200

    added tailwind + directus

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">SvelteKit + Directus + TailwindCSS!</h1>
+<h2 class="text-xl font-semibold text-gray-500">Documentation:</h2>
+<ul class="m-4 gap-3 flex flex-col">
+	<li>
+		<a
+			href="https://kit.svelte.dev"
+			target="_blank"
+			class="text-red-500 font-semibold hover:underline underline-offset-1 decoration-2"
+			>SvelteKit</a
+		>
+	</li>
+	<li>
+		<a
+			href="https://docs.directus.io"
+			target="_blank"
+			class="text-violet-700 font-semibold underline-offset-1 hover:underline decoration-2"
+			>Directus</a
+		>
+	</li>
+	<li>
+		<a
+			href="https://tailwindcss.com/docs/utility-first"
+			target="_blank"
+			class="text-sky-400 font-semibold underline-offset-1 hover:underline decoration-2"
+			>TailwindCSS</a
+		>
+	</li>
+</ul>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
